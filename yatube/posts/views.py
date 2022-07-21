d478bca40e2d5d37e8cd37b8e144cd0778f949a5
Антон Козлов <kozlov.7859@gmail.com>
2022-07-21 15:31:21 +0300

    2.2 push

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -15,7 +15,14 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.group.related_name.order_by('-pub_date')[:FILTER]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:FILTER]
+    # Попробовал напрямую запросить посты из группы.
+    # posts = Post.group.related_name.order_by('-pub_date')[:FILTER]
+    # при запуски сайта проблем не возникало.
+    # Во время запуска тестов возникла ошибка
+    # AttributeError:
+    # 'ForwardManyToOneDescriptor' object has no attribute 'related_name'
+
     context = {
         'group': group,
         'posts': posts,
